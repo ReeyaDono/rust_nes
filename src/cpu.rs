3d9f5752e3c13d0439f08a41ccc2ed2a732b46f2
ReeyaDono <metrytmc@gmail.com>
2023-02-18 20:28:52 +0700

    Implement 16 bit mem read and write

--- src/cpu.rs
+++ src/cpu.rs
@@ -16,6 +16,19 @@ impl CPU {
         self.memory[addr as usize] = data;
     }
 
+    fn mem_read_u16(&self, addr: u16) -> u16 {
+        let lo = self.mem_read(addr) as u16;
+        let hi = self.mem_read(addr+1) as u16;
+        (hi << 8) | lo
+    }
+
+    fn mem_write_u16(&self, addr: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xFF) as u8;
+        self.mem_write(addr, lo);
+        self.mem_write(addr+1, hi);
+    }
+
     pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
         self.run();
